de7c798bd3f94a240b6a2a2524c8175f6e2f7691
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2021-07-17 19:12:28 +0100

    main empty code

--- sumotest.py
+++ sumotest.py
@@ -18,49 +18,14 @@ traci.start(sumoCmd)
 print("Starting SUMO")
 traci.gui.setSchema("View #0", "real world")
 
-vehicles = traci.simulation.getLoadedIDList()
-for veh in range(0, len(vehicles)):
-    traci.vehicle.subscribeContext(vehicles[veh], tc.CMD_GET_VEHICLE_VARIABLE, 60, [tc.VAR_SPEED])
-
-
-# def random_go(vehicle):
-#     for k in range(0, len(vehicle)):
-#         if traci.vehicle.getSpeed(vehicle[k]) == 0:
-#             togo = randrange(0, len(vehicle))
-#             traci.vehicle.setSpeed(vehicle[togo], -1)
-
-def random_go(vehicle):
-    togo = randrange(0, len(vehicle))
-    if traci.vehicle.getStopState(vehicle[togo]) == 1:
-        traci.vehicle.resume(vehicle[togo])
-
-
 j = 0
 
 while j < 80:
     # this runs one simulation step
-    time.sleep(0.25)
+    time.sleep(0.1)
     traci.simulationStep()
 
     vehicles = traci.vehicle.getIDList()
-    for car in range(0, len(vehicles)):
-        neighbours = traci.vehicle.getContextSubscriptionResults(vehicles[car])
-
-        if neighbours:
-            neighbourcar = list(neighbours)
-            for i in range(0, len(neighbourcar)):
-                currentEdge = traci.vehicle.getRoadID(vehicles[car])
-                if traci.vehicle.getRoadID(neighbourcar[i]) != currentEdge:
-                    currentEdge = traci.vehicle.getRoadID(vehicles[car])
-                    fullCurrentEdge = currentEdge + "_0"
-                    distance = traci.lane.getLength(fullCurrentEdge)
-                    if traci.vehicle.getDistance(vehicles[car]) < distance:
-                        try:
-                            traci.vehicle.setStop(vehicles[car], traci.vehicle.getRoadID(vehicles[car]), distance, 0,
-                                                  10, 0)
-                        except traci.exceptions.TraCIException:
-                            pass
-            random_go(neighbourcar)
 
     j = j + 1
 
